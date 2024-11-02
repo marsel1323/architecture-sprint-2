c77eedcb78e9c08a82e8375a67b75c3803b4434b
mnabdrakhmanov <mnabdrakhmanov@avito.ru>
2024-11-02 17:54:37 +0300

    sprint_1: задание 3

---
+++ mongo-sharding-repl/scripts/2 fill.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###
+# Ожидание готовности mongos_router
+###
+
+echo "Ожидание готовности mongos_router..."
+until docker compose exec -T mongos_router mongo --port 27018 --quiet --eval "db.adminCommand('ping')" &> /dev/null; do
+  echo "mongos_router не готов, повторная проверка через 5 секунд..."
+  sleep 5
+done
+
+echo "mongos_router готов, начинаем настройку шардирования и заполнение базы."
+
+
+###
+# Добавление шардов и настройка шардирования
+###
+
+docker compose exec -T mongos_router mongo --port 27018 --quiet <<EOF
+sh.addShard("shard1/shard1-primary:27019");
+sh.addShard("shard1/shard1-secondary1:27020");
+sh.addShard("shard1/shard1-secondary2:27021");
+sh.addShard("shard2/shard2-primary:27022");
+sh.addShard("shard1/shard2-secondary1:27023");
+sh.addShard("shard1/shard2-secondary2:27024");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name": "hashed" });
+EOF
+
+###
+# Заполнение БД данными
+###
+
+docker compose exec -T mongos_router mongo --port 27018 --quiet <<EOF
+use somedb;
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age: i, name: "ly" + i});
+EOF
